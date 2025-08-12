b8d080e04ea3ccedb6a13ffb124ebdf3c4eedc33
Vinay Singhal <v.singhal@huulke.com>
2025-08-12 10:18:22 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+require('./db');
+const User = require('./users');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(bodyParser.json());
+
+app.get('/show', (req, res)=>{
+    res.status(200).send("Hello world");
+});
+
+app.put("/save", async (req,res)=>{
+    try{
+        // let user = new User();
+        const arr =  req.body;
+        arr.map(async(obj)=>{
+            let user = new User();
+            user.regno = obj.regno;
+            user.name = obj.name;
+            user.marks = obj.marks;
+            user = await user.save();
+        });
+        res.status(200).send({data: arr});
+    } catch(err) {
+        console.log(err);
+        res.status(500).send("Some Error");
+    }
+})
+
+app.listen(4000, ()=>{
+    console.log("server started and again");
+})
