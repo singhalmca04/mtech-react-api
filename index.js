f4997f896b18afc63bb1360f8678fcd4af34b573
Vinay Singhal <v.singhal@huulke.com>
2025-08-12 10:47:49 +0530

    first commit

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 require('./db');
 const User = require('./users');
+const cors = require("cors");
+app.use(cors());
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({
@@ -10,28 +12,25 @@ app.use(bodyParser.urlencoded({
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.status(200).send("Hello world");
 });
 
-app.put("/save", async (req,res)=>{
-    try{
-        // let user = new User();
-        const arr =  req.body;
-        arr.map(async(obj)=>{
-            let user = new User();
-            user.regno = obj.regno;
-            user.name = obj.name;
-            user.marks = obj.marks;
-            user = await user.save();
-        });
-        res.status(200).send({data: arr});
-    } catch(err) {
+app.post("/save", async (req, res) => {
+    try {
+        let { regno, name, marks } = req.body;
+        let user = new User();
+        user.regno = regno;
+        user.name = name;
+        user.marks = marks;
+        user = await user.save();
+        res.status(200).send({ data: user });
+    } catch (err) {
         console.log(err);
         res.status(500).send("Some Error");
     }
 })
 
-app.listen(4000, ()=>{
+app.listen(4000, () => {
     console.log("server started and again");
 })
